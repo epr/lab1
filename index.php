22981c202fb7aff70108b3b397ce801d222eba71
Eddy Proca <Eddy@Eddys-MacBook-Pro-2.local>
2014-11-12 19:53:26 +0100

    get course code, syllabus and intro

--- index.php
+++ index.php
@@ -1,26 +1,46 @@
 <?php
-require_once "html5/Parser.php";
+#error_reporting(E_ALL & ~E_WARNING);
+require_once "html5/Parser.php"; /*https://github.com/html5lib/html5lib-php*/
+echoHtmlStart();
 getCourses("/kurser/");
+echoHtmlEnd();
 
 function getCourses($url) {
     $url = "https://coursepress.lnu.se" . $url; //links are relative
     $xpath = xpathGet($url);
-    $courselist = $xpath->query("//ul[@id = 'blogs-list']//div[@class = 'item-title']/a[contains(@href, 'kurs')]");
-    foreach ($courselist as $course) {
+    $courseList = $xpath->query("//ul[@id = 'blogs-list']//div[@class = 'item-title']/a[contains(@href, 'kurs')]");
+    foreach ($courseList as $course) {
         doSomethingWithCourses($course);
     }
-    $nextpage = $xpath->query("//div[@id = 'blog-dir-pag-bottom']//a[contains(@class, 'next')]"); //find next page button
-    if ($nextpage->length > 0) { //check for last page
-        getCourses($nextpage->item(0)->getAttribute("href"));
+    $nextPage = $xpath->query("//div[@id = 'blog-dir-pag-bottom']//a[contains(@class, 'next')]"); //find next page button
+    if ($nextPage->length > 0) { //check for last page
+        getCourses($nextPage->item(0)->getAttribute("href"));
     }
 }
 
 function doSomethingWithCourses($course) {
     $courseName = $course->nodeValue;
     $courseLink = $course->getAttribute("href");
-    $xpath = html5($courseLink);
+    $xpath = xpathGet($courseLink);
+    $courseCodeList = $xpath->query("//div[@id = 'header-wrapper']//li");
+    $courseCode = $courseCodeList->item(2)->nodeValue;
+    $courseSyllabusList = $xpath->query("//ul[@class = 'sub-menu']//a[contains(@href, 'coursesyllabus')]");
+    if ($courseSyllabusList->length > 0) {
+        $courseSyllabus = $courseSyllabusList->item(0)->getAttribute("href");
+    }
+    else {
+        $courseSyllabus = "no information";
+    }
+    $courseIntroList = $xpath->query("//div[@class = 'entry-content'][0]//p");
+    if ($courseIntroList->length > 0) {
+        $courseIntro = $courseIntroList->item(0)->nodeValue;
+    }
+    else {
+        $courseIntro = "no information";
+    }
 
-    echo "<div>" . $courseName . " -> " . $courseLink . "</div>";
+    echo "<p>" . $courseCode . " <a href=\"" . $courseLink . "\">" . $courseName . "</a> <a href=\"" . $courseSyllabus . "\">Syllabus</a></p>
+    <p>" . $courseIntro . "</p>";
 }
 
 function curlGet($url) {
@@ -37,9 +57,9 @@ function curlGet($url) {
 function xpathGet($url) {
     $data = curlGet($url);
     $dom = new DOMDocument();
-    #libxml_use_internal_errors(true);
+    libxml_use_internal_errors(true);
     $result = $dom->loadHTML($data);
-    #libxml_clear_errors();
+    libxml_clear_errors();
     if ($result) {
         return (new DOMXPath($dom));
     }
@@ -58,4 +78,19 @@ function html5($url) {
     else {
         die("Fel vid inläsning av HTML");
     }
+}
+
+function echoHtmlStart() {
+    echo "<!doctype html>
+<html>
+<head>
+    <meta charset=\"utf-8\">
+</head>
+<body>
+    ";
+}
+
+function echoHtmlEnd() {
+    echo "</body>
+</html>";
 }
