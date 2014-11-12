8258ba8ac62319aeea5df80a84e3cfcad6635443
Eddy Proca <Eddy@Eddys-MacBook-Pro-2.local>
2014-11-12 16:00:43 +0100

    nodesValue test

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
-curl_get_request("https://coursepress.lnu.se/kurser/");
+$req = curl_get_request("http://coursepress.lnu.se/kurser/");
+$dom = new DOMDocument();
+$dom->loadHTML($req);
+$xpath = new DOMXPath($dom);
+$courselist = $xpath->query("//ul[@id = 'blogs-list']");
+foreach ($courselist as $course) {
+    echo($course->nodeValue);
+}
 function curl_get_request($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT,"eprcz09");
     $data = curl_exec($ch);
     curl_close($ch);
-    #var_dump($data);
+    return $data;
 }
