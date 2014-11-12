65a31cc5700212ade31dbc4b12940cf5aaac7616
Eddy Proca <Eddy@Eddys-MacBook-Pro-2.local>
2014-11-12 15:21:29 +0100

    curl dump

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-echo "test";
+<?php
+
+curl_get_request("https://coursepress.lnu.se/kurser/");
+function curl_get_request($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    #var_dump($data);
+}
